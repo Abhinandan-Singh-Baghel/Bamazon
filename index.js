fdcbec2a8e6c91b8eea871cffabc00f098f49106
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-02-28 23:57:37 +0530

    connection established to db

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 const mysql = require('mysql');
-require('dotenv').congif();
+require('dotenv').config();
 
 const password = process.env.PASSWORD;
+
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: password,
+    database: 'bamazon'
+});
+
+// Connect to the mysql server
+
+connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err);
+      return;
+    }
+    console.log('Connected to MySQL database');
+    
+    // You can now execute queries or perform other database operations here
+  });
